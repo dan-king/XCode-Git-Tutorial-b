9f45593fb27530a5ba11dde04c22cdf3cd1c9274
Dan King <dan.king@vptech.io>
2020-03-17 10:09:13 -0700

    Initial Commit

---
+++ XCode-Git-Tutorial-b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCode-Git-Tutorial-b
+//
+//  Created by Dan King on 3/17/20.
+//  Copyright © 2020 Viewpoint Technical LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
